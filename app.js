ef4e655a56e05020552c04ce67d28b5839a92061
Fredi <arro.fredi@gmail.com>
2019-05-02 17:16:24 +0300

    Added moving box that can be moved with arrows.

--- app.js
+++ app.js
@@ -3,58 +3,110 @@ window.addEventListener("load", function() {
 });
 
 // reference to canvas
-var canvas = document.getElementById("mycanvas");
-    var w = window.innerWidth-20 + "px";
-    var h = window.innerHeight-70 + "px";
-    var ctx = mycanvas.getContext('2d');
-    var snakeSize = 10;
-    var score = 0;
-    var snake;
-    var snakeSize = 10;
-    var food;
+var w = window.innerWidth-20 + "px";
+var h = window.innerHeight-70 + "px";
+var myGamePiece;
 
-function resizeCanvas() {
-    canvas.style.width = w;
-  // artifical delay so innerHeight is correct
-  setTimeout(function() {
-    canvas.style.height = h;
-  }, 0);
-};
+// function resizeCanvas() {
+//     canvas.style.width = w;
+//   // artifical delay so innerHeight is correct
+//   setTimeout(function() {
+//     canvas.style.height = h;
+//   }, 0);
+// };
 // So we fire it manually...
-resizeCanvas();
+// resizeCanvas();
 //source to canvas resizing function: https://blog.codepen.io/2013/07/29/full-screen-canvas/
 
-// functions for the D-pad buttons
-const softkeyCallback = {
-    left: function() { canvas.style.backgroundColor = "#2E4801"; },
-    right: function() { console.log('You click on ArrowRight') },
-    down: function() { console.log('You click on ArrowDown') },
-    up: function() { console.log('You click on ArrowUp') },
-};
-//function to react to kry presses
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+    start : function() {
+      this.canvas.style.width = w;
+      this.canvas.style.height = h;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+
+function moveLeft() {
+  myGamePiece.speedX = -1;
+}
+
+function moveRight() {
+   myGamePiece.speedX = 1;
+}
+
+function moveDown() {
+   myGamePiece.speedY = 1;
+}
+
+function moveUp() {
+   myGamePiece.speedY = -1;
+}
+
+//function to react to key presses
 function handleKeyDown(evt) {
     switch (evt.key) {
         case 'ArrowLeft':
             // Action case press left key
-            softkeyCallback.left();
+            moveLeft();
         break;
 
         case 'ArrowRight':
             // Action case press right key
-            softkeyCallback.right();
+             moveRight();
         break;
 
         case 'ArrowDown':
             // Action case press center key
-            softkeyCallback.down();
+             moveDown();
         break;
 
         case 'ArrowUp':
             // Action case press left key
-            softkeyCallback.up();
+             moveUp();
         break;
     }
 };
 //listener that listens for button presses
 document.addEventListener('keydown', handleKeyDown);
 //source to button listener: https://developer.kaiostech.com/first-app/softkeys
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
